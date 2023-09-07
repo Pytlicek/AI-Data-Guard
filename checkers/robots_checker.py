e26f4550034ce69549159bfc441a990ee14cb866
Tomas-Pytel <Tomas.Pytel@ibm.com>
2023-09-07 09:02:45 +0200

    Added some domains to domains.txt Refactoring of robots_checker.py

--- checkers/robots_checker.py
+++ checkers/robots_checker.py
@@ -8,11 +8,26 @@ ssl_context = ssl._create_unverified_context()
 timeout_seconds = 2
 
 
-def check_robots_domain(domain_name):
-    print("Checking Domain:", domain_name)
+def get_robots_txt_on_tld(domain_name):
     robots_url = f'https://{domain_name}/robots.txt'  # Use HTTPS
-    print("robots_url:", robots_url)
+    print("Checking Domain:", domain_name, robots_url)
 
+    # Fetch the content of robots.txt
+    try:
+        with urllib.request.urlopen(robots_url, timeout=timeout_seconds,
+                                    context=ssl_context) as response:
+            lines = response.read().decode().splitlines()
+        return lines
+
+    except urllib.error.HTTPError:
+        print(f"Failed to fetch robots.txt for {domain_name}")
+    except socket.timeout:
+        print("Timeout occurred")
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+
+
+def check_robots_domain(domain_name):
     check_response = {
         "gptbot_is_allowed": False,
         "gptbot_definition_explicit": False
@@ -21,45 +36,31 @@ def check_robots_domain(domain_name):
     # Flags to indicate if we are in the GPTBot section or the wildcard section
     in_gptbot_section = False
     in_wildcard_section = False
+    wildcard_rules = []
 
-    # Fetch the content of robots.txt
-    try:
-        with urllib.request.urlopen(robots_url, timeout=timeout_seconds,
-                                    context=ssl_context) as response:
-            lines = response.read().decode().splitlines()
+    robots_rules = get_robots_txt_on_tld(domain_name)
+    # Iterate through the lines of robots.txt
+    for line in robots_rules:
+        line = line.strip()
+        if line == "User-agent: GPTBot":
+            in_gptbot_section = True
+            in_wildcard_section = False
+        elif line == "User-Agent: *":
+            in_wildcard_section = True
+            in_gptbot_section = False
+        elif line.startswith("User-agent:") and (
+                in_gptbot_section or in_wildcard_section):
+            break
+        elif in_gptbot_section:
+            print(">", line)  # Print the Allow/Disallow lines for GPTBot
+        elif in_wildcard_section:
+            wildcard_rules.append(line)
 
-        wildcard_rules = []
-
-        # Iterate through the lines of robots.txt
-        for line in lines:
-            line = line.strip()
-            if line == "User-agent: GPTBot":
-                print(f"Checking rules for domain_name: {robots_url}")
-                in_gptbot_section = True
-                in_wildcard_section = False
-            elif line == "User-Agent: *":
-                in_wildcard_section = True
-                in_gptbot_section = False
-            elif line.startswith("User-agent:") and (
-                    in_gptbot_section or in_wildcard_section):
-                break
-            elif in_gptbot_section:
-                print(">", line)  # Print the Allow/Disallow lines for GPTBot
-            elif in_wildcard_section:
-                wildcard_rules.append(line)
-
-        # If no specific rules for GPTBot were found, print the wildcard rules
-        # if not in_gptbot_section and wildcard_rules:
-        #     print("No specific rules for GPTBot. Applying wildcard rules:")
-        #     for rule in wildcard_rules:
-        #         print(rule)
-
-    except urllib.error.HTTPError:
-        print(f"Failed to fetch robots.txt for {domain_name}")
-    except socket.timeout:
-        print("Timeout occurred")
-    except Exception as e:
-        print("An unexpected error occurred:", e)
+    # If no specific rules for GPTBot were found, print the wildcard rules
+    # if not in_gptbot_section and wildcard_rules:
+    #     print("No specific rules for GPTBot. Applying wildcard rules:")
+    #     for rule in wildcard_rules:
+    #         print(rule)
 
     if in_gptbot_section:
         check_response['gptbot_is_allowed'] = False
