365a5ece56bc5f8dc66fdfef91541eebfb8d1e0b
Tomas-Pytel <Tomas.Pytel@ibm.com>
2023-09-12 11:27:36 +0200

    Refactor api/views and update endpoint descriptions
    The changes in api/views.py involve mainly the removal of /search/{str:search_query} endpoint and the import statements' relocation at the top for better readability. Additionally, descriptions of /check/robots/by_domain_name and /check/robots/by_url endpoints have been updated to reflect their functionalities accurately.
    
    These changes will help in providing a better understanding of the API's endpoint functionalities and make the code structure more organized. The search endpoint removal reflects its redundancy or non-requirement in the current application setup.

--- api/views.py
+++ api/views.py
@@ -1,40 +1,16 @@
+from checkers.robots_checker import check_robots_domain, check_robots_url
+from dataguard.settings import env
 from django.shortcuts import redirect
 from ninja import NinjaAPI
 from . import schemas
-from dataguard.settings import env
 
-api = NinjaAPI(title="AI DataGuard API", description="DG API Endpoints", version="1.0.0")
+api = NinjaAPI(title="AI DataGuard API", description="DG API Endpoints",
+               version="1.0.0")
 
 
 @api.get("/", include_in_schema=False)
 def hello(request):
     return redirect("./docs#/")
-
-
-@api.get(
-    "/search/{str:search_query}",
-    response=schemas.SearchResult,
-    tags=["search"],
-)
-def get_search(request, search_query: str):
-    """
-    **Search for text**
-    <br><br>
-    **Try fe.** `Test`
-    """
-    example_variable_from_env = env(
-        "API_USERNAME",
-        default="ENV1",
-    )
-    search_result = {
-        "search_query": search_query,
-        "search_response": f"example search_response for: {search_query}",
-        "search_metadata": example_variable_from_env,
-    }
-    return search_result
-
-
-from checkers.robots_checker import check_robots_domain, check_robots_url
 
 
 @api.get(
@@ -44,7 +20,7 @@ from checkers.robots_checker import check_robots_domain, check_robots_url
 )
 def get_check_robots_by_domain_name(request, domain_name: str):
     """
-    **Search for text**
+    **Endpoint to check robots.txt rules for a given Domain name**
     <br><br>
     **Try fe.** `amazon.com`
     """
@@ -66,7 +42,7 @@ def get_check_robots_by_domain_name(request, domain_name: str):
 )
 def get_check_robots_by_url(request, url: str):
     """
-    **Search for text**
+    **Endpoint to check robots.txt rules for a given URL.**
     <br><br>
     **Try fe.** `https://amazon.com/`
     """
